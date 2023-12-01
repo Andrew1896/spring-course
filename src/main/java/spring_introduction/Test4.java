1b18c932ea77ba4ba0b93cd8ad4f94996250b142
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-01 21:55:36 +0100

    Modificari in fisierul Dog, applicationContext2.xml, Test4 si crearea fisierului Test5

--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -7,13 +7,13 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Lancelot");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Artik");
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Lancelot");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Artik");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
 //        System.out.println("Variabilele se bazaeaza pe unul si acelasi obiect? "
 //                + (myDog == yourDog));
