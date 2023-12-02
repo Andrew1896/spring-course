cbfbdaa0660dae8702db57184dcfa3b5cd9f6106
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-02 19:03:10 +0100

    Modificari in fisierul Person si Dog

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,10 +1,13 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
@@ -19,6 +22,7 @@ public class Person {
         System.out.println("Person bean is created");
     }
     @Autowired
+    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
