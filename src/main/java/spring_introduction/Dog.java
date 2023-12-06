26edace72b208073e7049ee28533945c018c3973
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-06 23:25:55 +0100

    Crearea adnotarilor PostConstruct si PreDestroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,10 +1,11 @@
 package spring_introduction;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope ("singleton")
 public class Dog implements Pet {
 //    private String name;
 
@@ -18,11 +19,13 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
-    protected void init(){
+    @PostConstruct
+    protected void init() {
         System.out.println("Class Dog: init method");
     }
 
-    private void destroy(){
+    @PreDestroy
+    private void destroy() {
         System.out.println("Class Dog: destroy method");
     }
 //
