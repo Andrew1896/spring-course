db759382d4e20813008a1557e92537a23830a255
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-11 23:21:15 +0100

    Primirea obiectului din baza fara ajutorul ID

---
+++ src/main/java/hibernate_test/entity/Test3.java
@@ -0,0 +1,30 @@
+package hibernate_test.entity;
+
+import javax.security.auth.login.Configuration;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cgf.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            List<Employee> emps = session.createQuery("from Employee" +
+                            "where name = 'Alexandr' AND salary>650")
+                    .getResultList();
+
+            for (Employee e : emps)
+                System.out.println(e);
+
+            session = getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
