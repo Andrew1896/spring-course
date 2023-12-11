0cb797119a8a270a56c35d708c53ba0304a29872
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-11 23:12:00 +0100

    Primirea obiectului din baza cu ajutorul ID

---
+++ src/main/java/hibernate_test/entity/Test2.java
@@ -0,0 +1,33 @@
+package hibernate_test.entity;
+
+import javax.security.auth.login.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cgf.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Andrei", "Cataraga", "IT", 1000);
+            session.beginTransaction();
+            session.save(emp);
+            session.beginTransaction().commit();
+
+            int myID = emp.getId();
+            session = factory.getCurrentSession();
+            session = beginTransaction();
+            Employee employee = session.get(Employee.class, myID);
+            session = getTransaction().commit();
+            System.out.println(employee);
+
+
+            System.out.println("Done!");
+
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
