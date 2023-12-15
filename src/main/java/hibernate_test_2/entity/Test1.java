e774ed8c3c04c7e5bc9a4dfba22573177f0679fe
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-15 23:45:03 +0100

    Obtinerea si eliminarea din baza de date a obiectelor, relatia One-to-One

--- src/main/java/hibernate_test_2/entity/Test1.java
+++ src/main/java/hibernate_test_2/entity/Test1.java
@@ -11,24 +11,49 @@ public class Test1 {
                 .addAnnotatedClass(Employee.class)
                 .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
+
+        Session session = null;
         try {
-            Session session = factory.getCurrentSession();
-            Employee employee = new Employee("Andrei", "Cataraga", "IT", 500);
-            Detail detail = new Detail("Chisinau", "068157232", "andreicataraga@gmail.com");
+            // Adaugarea obiectelor in baza de date
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Andrei", "Cataraga", "IT", 500);
+//            Detail detail = new Detail("Chisinau", "068157232", "andreicataraga@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.set(employee);
+//
+//
+//            session.beginTransaction().commit();
+//            System.out.println("Done!");
 
-            employee.setEmpDetail(detail);
+
+              // Obtinea obiectelor din baza de date
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Employee emp = session.get(Employee.class, 10);
+//            System.out.println(emp.getEmpDetail());
+//
+//
+//            session.beginTransaction().commit();
+//            System.out.println("Done!");
+
+
+            // Eliminarea obiectelor din baza de date
+            session = factory.getCurrentSession();
+
             session.beginTransaction();
-
-            session.set(employee);
-
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
 
             session.beginTransaction().commit();
             System.out.println("Done!");
 
 
-
-
         } finally {
+            session.close();
             factory.close();
         }
     }
