d0055d3d127e7ffcec89a90245d741c29a6e198a
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-26 19:37:04 +0200

    Add sql scripts insert employees

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -14,3 +14,22 @@ CREATE TABLE details (
 );
 
 USE my_db;
+
+select * from employees;
+select * from details;
+
+-- Inserare detalii
+INSERT INTO details (detail_info) VALUES
+('Detaliu 1'),
+('Detaliu 2'),
+('Detaliu 3'),
+('Detaliu 4'),
+('Detaliu 5');
+
+-- Inserare angajați
+INSERT INTO employees (name, surname, department, salary, details_id) VALUES
+('Angajat 1', 'Prenume1', 'Departament1', 50000, 1),
+('Angajat 2', 'Prenume2', 'Departament2', 55000, 2),
+('Angajat 3', 'Prenume3', 'Departament1', 60000, 3),
+('Angajat 4', 'Prenume4', 'Departament3', 52000, 4),
+('Angajat 5', 'Prenume5', 'Departament2', 58000, 5);
